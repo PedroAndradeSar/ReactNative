98de986fbb385354aa1d5325d7158596f7844715
Pedro Henrique <phandradesa@gmail.com>
2023-07-27 21:54:54 -0300

    ReactNative Aula 01

--- App.js
+++ App.js
@@ -1,20 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>hello world</Text>
       <StatusBar style="auto" />
     </View>
-  );
+  );e
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "blackr",
+    alignItems: "center",
+    justifyContent: "center",
   },
+  text: {
+    color: "#fff",
+    fontSize: 30,
+
+  }
 });
